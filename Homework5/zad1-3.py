7b3c94e0cf0a1adbc28abe1904ec79b70242ee6e
Marcin Wałachowski <mwalachowski99@gmail.com>
2022-04-14 17:21:56 +0200

    Finished 5th homework

--- Homework5/zad1-3.py
+++ Homework5/zad1-3.py
@@ -1,11 +1,10 @@
 import pandas as pd
-import datetime
 
+# zad 1
 PATH='Homework5/orders.csv'
 
 df=pd.read_csv(PATH)
-print('Before:')
-print('Info :\n', df.info())
+print('Info :\n', df.info)
 print('\nDescribe :\n', df.describe())
 print('\nHead :\n', df.head(5))
 
@@ -44,4 +43,30 @@ df['tshirt_type']=df['tshirt_category'].apply(lambda x: get_type_from_category(x
 df['tshirt_colour']=df['tshirt_category'].apply(lambda x: get_colour_from_category(x))
 df['tshirt_size']=df['tshirt_category'].apply(lambda x: get_size_from_category(x))
 # f)
-#print(df.where(df.loc[df['order_date'].between(datetime.date(2014, 12, 31), datetime.date(2016, 8, 2))]))
+mask = (df['order_date'] > '31/12/2016') & (df['order_date'] <= '02/08/2016')
+print(df.loc[mask])
+
+# zad 2
+
+PATH2='Homework5/customers.csv'
+
+df2=pd.read_csv(PATH2)
+
+# a)
+print('\nColumns :\n', df2.columns)
+# b)
+print('Size of data from first exercise: ', df.size, df.shape)
+print('Size of dataframe from second exercise: ', df2.size, df2.shape)
+
+print('Size in memory of dataframe from first exercise: ' , df.memory_usage(index=True).sum(), ' bytes')
+print('Size in memory of dataframe from second exercise: ', df2.memory_usage(index=True).sum(), ' bytes')
+# c)
+df2.rename(columns = {'customerID':'customer_id'}, inplace = True)
+# d)
+df_merged = df.merge(df2,on = 'customer_id')
+print(df_merged)
+'''
+Do polączenia tych dwóch ramek użyłem funkcji merge, powodem mojego wyboru jest to, że ta funkcja pozwala na łączenie dwóch ramek na podstawie ich części wspólnej ( customer_id)
+'''
+# zad 3
+df.to_csv('Homework5/output.csv', index=False)
